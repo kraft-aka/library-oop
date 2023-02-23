f33126dd4e315f2643c013577626c9283027b413
kubat <kubat.aiupov@gmail.com>
2023-02-23 20:25:40 +0100

    edit Book class

--- app.js
+++ app.js
@@ -27,10 +27,16 @@ class Media {
 }
 
 class Book extends Media {
-  constructor(title, author, pages, ratings, isCheckedOut) {
-    super(title, ratings, isCheckedOut);
+  constructor(title, author, pages) {
+    super(title);
     this._author = author;
     this._pages = pages;
+  }
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
   }
 }
 
